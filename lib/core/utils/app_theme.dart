4a97c0b552bf5ef54bcdfd47248b80692ab3bd18
marksherif105 <domanictoretto95@gmail.com>
2026-09-18 19:40:19 +0300

    Updates

--- lib/core/utils/app_theme.dart
+++ lib/core/utils/app_theme.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
+  get darktheme => null;
+
+  get lighttheme => null;
+
   ThemeData lightTheme() {
     return ThemeData(brightness: Brightness.light);
   }
